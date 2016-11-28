3e452e026699055345063e4be46fa0bc4e595a6d
Maximilian Darr <maximiliandarr@Maximilians-MacBook-Pro.local>
2016-11-28 14:58:32 -0500

    Making Menu and versions separate

---
+++ Pong/game.py
@@ -0,0 +1,102 @@
+import pygame
+import random
+pygame.font.init()
+
+class MenuItem(pygame.font.Font):
+    def __init__(self, text, font=None, font_size=30,
+                 font_color=(255, 255, 255), pos_x=0, pos_y=0):
+        pygame.font.Font.__init__(self, font, font_size)
+        self.text = text
+        self.font_size = font_size
+        self.font_color = font_color
+        self.label = self.render(self.text, 1, self.font_color)
+        self.width = self.label.get_rect().width
+        self.height = self.label.get_rect().height
+        self.dimensions = (self.width, self.height)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.position = pos_x, pos_y
+ 
+    def set_position(self, x, y):
+        self.position = (x, y)
+        self.pos_x = x
+        self.pos_y = y
+ 
+    def set_font_color(self, rgb_tuple):
+        self.font_color = rgb_tuple
+        self.label = self.render(self.text, 1, self.font_color)
+ 
+    def is_mouse_selection(self, posx, posy):
+        #pos = pygame.mouse.get_pos()
+        if (posx >= self.pos_x and posx <= self.pos_x + self.width) and \
+            (posy >= self.pos_y and posy <= self.pos_y + self.height):
+                return True
+        return False
+ 
+ 
+class GameMenu():
+    def __init__(self, screen, items, bg_color=(0,0,0), font=None, font_size=30,
+                    font_color=(255, 255, 255)):
+        self.screen = screen
+        self.scr_width = self.screen.get_rect().width
+        self.scr_height = self.screen.get_rect().height
+        self.funcs = funcs
+ 
+        self.bg_color = bg_color
+        self.clock = pygame.time.Clock()
+ 
+        self.items = []
+        for index, item in enumerate(items):
+            menu_item = MenuItem(item)#, '/home/nebelhom/.fonts/SHOWG.TTF')
+ 
+            # t_h: total height of text block
+            t_h = len(items) * menu_item.height
+            pos_x = (self.scr_width / 2) - (menu_item.width / 2)
+            pos_y = (self.scr_height / 2) - (t_h / 2) + ((index * 2) + index * menu_item.height)
+ 
+            menu_item.set_position(pos_x, pos_y)
+            self.items.append(menu_item)
+ 
+    def run(self):
+        mainloop = True
+        while mainloop:
+            # Limit frame speed to 50 FPS
+            self.clock.tick(50)
+            mpos = pygame.mouse.get_pos()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    mainloop = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for item in self.items:
+                        if item.is_mouse_selection(posx=mpos[0],posy=mpos[1]):
+                            self.funcs[item.text]()
+            # Redraw the background
+            self.screen.fill(self.bg_color)
+ 
+            for item in self.items:
+                pos = pygame.mouse.get_pos()
+
+                if item.is_mouse_selection(posx=pos[0],posy=pos[1]):
+                    item.set_font_color((255, 0, 0))
+                    item.set_italic(True)
+                else:
+                    item.set_font_color((255, 255, 255))
+                    item.set_italic(False)
+                self.screen.blit(item.label, item.position)
+ 
+            pygame.display.flip()
+ 
+ 
+if __name__ == "__main__":
+    # Creating the screen
+    screen = pygame.display.set_mode((640, 480), 0, 32)
+ 
+    menu_items = ('EASY', 'HARD')
+    def hard():
+        print("HARD")
+    def easy():
+        print("EASY")
+    funcs = {'EASY': easy, 'HARD': hard}
+    pygame.display.set_caption('Game Menu')
+    gm = GameMenu(screen, menu_items)
+    gm.run()
